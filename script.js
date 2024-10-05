cdd121d1d14b23316a66b2be9b720dcf61ba2183
samna <samnasaif666@gmail.com>
2024-10-05 09:22:23 +0530

    nodejsexample -4-10

---
+++ script.js
@@ -0,0 +1,15 @@
+// console.log("Running")
+// const array = ["a" , "b" , "c"];
+// console.log(array);
+// const newArray = [...array , "d"];
+// console.log(newArray)
+
+// const interval = setInterval(() =>{
+//     console.log("Running");
+// },1000);
+
+// setTimeout(() => {
+//    clearInterval(interval);
+
+// },3000);
+
